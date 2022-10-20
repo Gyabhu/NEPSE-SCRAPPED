d8fc45047eafc85e332946b4c5e22c32363b9432
Gyabhu <gabriel.moktan777@gmail.com>
2022-10-20 15:09:52 +0545

    Converted to CSV

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
-data=[]
-datas=[]
+import csv
+data = []
+datas = []
 html_text = requests.get("http://www.nepalstock.com/todaysprice").text
 soup = BeautifulSoup(html_text, 'lxml')
 stocks = soup.find_all('tr')
 
-for stock in stocks:
-    x = stock.text.replace(' ','')
-    data.append(x)
-for i in data:
-    datas.append(i.split('\n')[1:-1])
 
-del datas[0]
-del datas[-4]
-blank= ''
-clean_data = [[ele for ele in spaces if ele != blank] for spaces in datas]
-print(clean_data)
+for stock in stocks:
+    x = stock.text
+    data.append(x)
+# print(data)
+
+for i in data:
+    datas.append(i.split('\n'))
+
+del datas[0] #filtered unwanted info
+del datas[-4] #filered unwanted info
+
+
+file = open("NEPSE.csv",'w')
+write = csv.writer(file)
+for i in datas:
+    write.writerow(i)
+file.close()
