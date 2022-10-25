f5c3e73c30e20b595670c1b899fb8ff0dcfb6501
Gyabhu <gabriel.moktan777@gmail.com>
2022-10-25 17:32:51 +0545

    Cleaned data with Bar Graph(Incomplete)

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ for i in data:
     datas.append(i.split('\n')[1:-1])
 
 del datas[0]  # filtered unwanted info
-del datas[-4]  # filtered unwanted info
-for v in datas[1:-3]:  # filtered unwanted info
+del datas[-4:]  # filtered unwanted info
+for v in datas[1:]:  # filtered unwanted info
     v.pop()
     v.pop(-1)
-
+print(datas)
 # csv conversion
 file = open("NEPSE.csv", 'w')
 write = csv.writer(file)
@@ -31,6 +31,15 @@ for i in datas:
     write.writerow(i)
 file.close()
 
+
 # pandas visualization
 df = pd.read_csv('NEPSE.csv', encoding='iso-8859-1')
-print(df)
+
+
+plt.style.use('_mpl-gallery')
+x = list(df['Traded Companies'])
+y = list(df['No. Of Transaction'])
+
+plt.bar(x, y)
+plt.show()
+
